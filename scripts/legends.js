eaf1f81efa05993f2183d7a8c63844c60dfab12c
Niinty <niinty@gmail.com>
2026-05-23 16:39:23 -0300

    Update legends.js

--- scripts/legends.js
+++ scripts/legends.js
@@ -17,8 +17,8 @@ const LEGENDS_LIST = [
         // Duração da janela de batalha em ms (72h)
         windowMs: 72 * 60 * 60 * 1000,
         // Multiplicadores de batalha (apenas durante a luta, não altera pkmn permanentemente)
-        hpMultiplier: 5.0,
-        damageMultiplier: 8.0,
+        hpMultiplier: 2.0,
+        damageMultiplier: 2.0,
         // Nível com que o lendário aparece na batalha
         battleLevel: 150,
         // Dificuldade da área (usa constantes do areasDictionary)
