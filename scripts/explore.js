e0a74d198a4a09e3ecf10c99ee67b5b2325ae2bf
Niinty <niinty@gmail.com>
2026-05-25 07:53:46 -0300

    Update explore.js

--- scripts/explore.js
+++ scripts/explore.js
@@ -8,7 +8,7 @@ saved.currentSeason = undefined
 
 // Default fatigue multiplier: reduce fatigue by 25% (0.75)
 // To change for testing, edit this value here.
-let testFatigueMultiplier = 1
+let testFatigueMultiplier = 0.8
 
 let team = {}
 
