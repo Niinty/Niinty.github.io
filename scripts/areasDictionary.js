6d0eb52b415390dbcb2bb8cac8d0fc2ccf02ab85
Niinty <niinty@gmail.com>
2026-05-25 11:37:51 -0300

    Update areasDictionary.js

--- scripts/areasDictionary.js
+++ scripts/areasDictionary.js
@@ -6448,6 +6448,11 @@ for (const i in areas){
         areas[i].unlockRequirement = function() { return areas.vsGymLeaderLtsurge.defeated }
     }
 
+    if (areas[i].type == "wild" && areas[i].level == wildAreaLevel5) {
+        areas[i].unlockDescription = `Defeat Gym Leader Erika in Ginásios to unlock`
+        areas[i].unlockRequirement = function() { return areas.vsGymLeaderErika.defeated }
+    }
+
 }
 
 
