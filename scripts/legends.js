b41c899781ce3d8a12c868740521fcbbf3c43501
Niinty <niinty@gmail.com>
2026-05-28 08:30:53 -0300

    Update legends.js

--- scripts/legends.js
+++ scripts/legends.js
@@ -115,6 +115,8 @@ const LEGENDS_LIST = [
         drops: [
             { itemId: "goldenBottleCap", amount: 1, chance: 0.75 },
             { itemId: "blackApricorn", amount: 1, chance: 0.05 },
+            { itemId: "mewtwoniteX", amount: 1, chance: 0.005 },
+            { itemId: "mewtwoniteY", amount: 1, chance: 0.005 },
         ],
         // Background da área de batalha
         background: "cave",
